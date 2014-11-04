5a0097cec3c5d847f187c3b57593f1c68f3eb77d
Vasiliy Blazhnov <vasiliy.blazhnov@gmail.com>
2014-11-04 22:36:30 +0500

    Добавление распознавания лиц в форму обучения

--- FaceRecognition/FormTraining.Designer.cs
+++ FaceRecognition/FormTraining.Designer.cs
@@ -13,6 +13,7 @@
 		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
 		protected override void Dispose(bool disposing)
 		{
+			ReleaseData();
 			if (disposing && (components != null))
 			{
 				components.Dispose();
@@ -28,11 +29,122 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.btnAdd = new System.Windows.Forms.Button();
+			this.btnAddAll = new System.Windows.Forms.Button();
+			this.btnPrev = new System.Windows.Forms.Button();
+			this.btnNext = new System.Windows.Forms.Button();
+			this.btnRec = new System.Windows.Forms.Button();
+			this.btnRecMulty = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(3, 2);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Location = new System.Drawing.Point(664, 12);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox2.TabIndex = 2;
+			this.pictureBox2.TabStop = false;
+			// 
+			// btnAdd
+			// 
+			this.btnAdd.Location = new System.Drawing.Point(664, 118);
+			this.btnAdd.Name = "btnAdd";
+			this.btnAdd.Size = new System.Drawing.Size(100, 23);
+			this.btnAdd.TabIndex = 3;
+			this.btnAdd.Text = "Добавить";
+			this.btnAdd.UseVisualStyleBackColor = true;
+			// 
+			// btnAddAll
+			// 
+			this.btnAddAll.Location = new System.Drawing.Point(664, 176);
+			this.btnAddAll.Name = "btnAddAll";
+			this.btnAddAll.Size = new System.Drawing.Size(100, 23);
+			this.btnAddAll.TabIndex = 4;
+			this.btnAddAll.Text = "Добавить все";
+			this.btnAddAll.UseVisualStyleBackColor = true;
+			// 
+			// btnPrev
+			// 
+			this.btnPrev.Location = new System.Drawing.Point(664, 147);
+			this.btnPrev.Name = "btnPrev";
+			this.btnPrev.Size = new System.Drawing.Size(40, 23);
+			this.btnPrev.TabIndex = 5;
+			this.btnPrev.Text = "<<";
+			this.btnPrev.UseVisualStyleBackColor = true;
+			// 
+			// btnNext
+			// 
+			this.btnNext.Location = new System.Drawing.Point(724, 147);
+			this.btnNext.Name = "btnNext";
+			this.btnNext.Size = new System.Drawing.Size(40, 23);
+			this.btnNext.TabIndex = 6;
+			this.btnNext.Text = ">>";
+			this.btnNext.UseVisualStyleBackColor = true;
+			// 
+			// btnRec
+			// 
+			this.btnRec.Location = new System.Drawing.Point(664, 430);
+			this.btnRec.Name = "btnRec";
+			this.btnRec.Size = new System.Drawing.Size(100, 23);
+			this.btnRec.TabIndex = 7;
+			this.btnRec.Text = "Записать";
+			this.btnRec.UseVisualStyleBackColor = true;
+			// 
+			// btnRecMulty
+			// 
+			this.btnRecMulty.Location = new System.Drawing.Point(664, 459);
+			this.btnRecMulty.Name = "btnRecMulty";
+			this.btnRecMulty.Size = new System.Drawing.Size(100, 23);
+			this.btnRecMulty.TabIndex = 8;
+			this.btnRecMulty.Text = "Записать 10";
+			this.btnRecMulty.UseVisualStyleBackColor = true;
+			// 
+			// FormTraining
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "FormTraining";
+			this.ClientSize = new System.Drawing.Size(776, 507);
+			this.Controls.Add(this.btnRecMulty);
+			this.Controls.Add(this.btnRec);
+			this.Controls.Add(this.btnNext);
+			this.Controls.Add(this.btnPrev);
+			this.Controls.Add(this.btnAddAll);
+			this.Controls.Add(this.btnAdd);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "FormTraining";
+			this.Text = "Обучение";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormTraining_FormClosing);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Button btnAdd;
+		private System.Windows.Forms.Button btnAddAll;
+		private System.Windows.Forms.Button btnPrev;
+		private System.Windows.Forms.Button btnNext;
+		private System.Windows.Forms.Button btnRec;
+		private System.Windows.Forms.Button btnRecMulty;
+
 	}
 }
--- FaceRecognition/FormTraining.cs
+++ FaceRecognition/FormTraining.cs
@@ -1,19 +1,120 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
 using System.Windows.Forms;
+using System.Xml;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
 
 namespace FaceRecognition
 {
 	public partial class FormTraining : Form
 	{
-		public FormTraining()
+		private const int MULTY_FACE_COUNT = 10;
+
+		private enum RecordState
+		{
+			Rec,
+			RecMulty,
+			Stop
+		}
+
+		private Capture capture = null;
+		private CascadeClassifier haar = null;
+
+		private Image<Bgr, Byte> frame = null;
+		private Image<Gray, Byte> grayFrame = null;
+		private Image<Gray, Byte> currentFaceGray = null;
+
+		private List<Image<Gray, Byte>> faces = new List<Image<Gray, byte>>();
+		private int currentFacePos = 0;
+		private RecordState currentState = RecordState.Stop;
+
+		public FormTraining(CascadeClassifier haarFace)
 		{
 			InitializeComponent();
+			haar = haarFace;
+
+			InitializeCapture();
+		}
+
+		public void InitializeCapture()
+		{
+			capture = new Capture();
+
+			Application.Idle += ProcessFrame;
+		}
+
+		public void StopCapture()
+		{
+			Application.Idle -= ProcessFrame;
+			capture.Dispose();
+		}
+
+		private void ProcessFrame(object sender, EventArgs e)
+		{
+			frame = capture.QueryFrame().ToImage<Bgr, Byte>().Resize(320, 240, Emgu.CV.CvEnum.Inter.Cubic);
+
+			if (currentState == RecordState.Stop)
+			{
+				pictureBox1.Image = frame.ToBitmap();
+				return;
+			}
+
+			grayFrame = frame.Convert<Gray, Byte>();
+
+			Rectangle[] facesDetected = haar.DetectMultiScale(grayFrame, 1.2, 10, new Size(50, 50), Size.Empty);
+
+			if (facesDetected == null || facesDetected.Length == 0)
+			{
+				return;
+			}
+
+			Rectangle face = facesDetected[0];
+
+			face.X += (int)(face.Height * 0.15);
+			face.Y += (int)(face.Width * 0.22);
+			face.Height -= (int)(face.Height * 0.3);
+			face.Width -= (int)(face.Width * 0.35);
+
+			currentFaceGray = grayFrame.Copy(face).Resize(100, 100, Emgu.CV.CvEnum.Inter.Cubic);
+			currentFaceGray._EqualizeHist();
+
+			frame.Draw(face, new Bgr(Color.Red), 2);
+
+			pictureBox2.Image = currentFaceGray.ToBitmap();
+
+			switch (currentState)
+			{
+				case RecordState.Rec:
+					currentState = RecordState.Stop;
+					break;
+
+				case RecordState.RecMulty:
+					faces.Add(currentFaceGray);
+					if (faces.Count == MULTY_FACE_COUNT)
+					{
+						currentState = RecordState.Stop;
+					}
+					break;
+			}
+		}
+
+		private void ReleaseData()
+		{
+			if (capture != null)
+			{
+				StopCapture();
+			}
+		}
+
+		private void FormTraining_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			FormRecognizer.Instance.InitializeCapture();
 		}
 	}
 }
